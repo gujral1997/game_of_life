6c42f496ae2b248fdc00d9aed4cd0c4c3fd0f27f
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-05 12:25:37 +0530

    first working version of game created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ButtonToolbar,MenuItem,DropdownButton} from 'react-bootstrap';
 import './index.css';
 
 class Box extends React.Component{
   selectBox=()=>
   {
-    this.props.selectBox(this.props.row,this.props.col)
+    this.props.selectBox(this.props.row,this.props.col);
   }
   render()
   {
@@ -23,7 +24,7 @@ class Box extends React.Component{
 class Grid extends React.Component {
   render()
     {
-      const width=(this.props.cols * 16)+1;
+      const width=(this.props.cols * 14);
             var rowsArr= [];
             var boxClass="";
             for(var i=0;i<this.props.rows;i++)
@@ -39,19 +40,58 @@ class Grid extends React.Component {
                     boxId={boxId}
                     row={i}
                     col={j}
-                    slectBox={this.props.selectBox}
+                    selectBox={this.props.selectBox}
                   />
                 );
               }
             }
             return(
-              <div calssName="grid" style={{width:width}}>
+              <div className="grid" style={{width:width}}>
                 {rowsArr}
               </div>
             );
     }
   }
-
+class Buttons extends React.Component{
+  handleSelect=(evt)=>{
+    this.props.gridSize(evt);
+  }
+  render(){
+    return(
+      <div className="center">
+        <ButtonToolbar>
+          <button className="btn btn-default" onClick={this.props.playButton}>
+            Play
+          </button>
+          <button className="btn btn-default" onClick={this.props.pauseButton}>
+            Pause
+          </button>
+          <button className="btn btn-default" onClick={this.props.clear}>
+            Clear
+          </button>
+          <button className="btn btn-default" onClick={this.props.fast}>
+            Fast
+          </button>
+          <button className="btn btn-default" onClick={this.props.slow}>
+            Slow
+          </button>
+          <button className="btn btn-default" onClick={this.props.seed}>
+            Seed
+          </button>
+          <DropdownButton
+            title="Grid Size"
+            id="size-menu"
+            onSelect={this.handleSelect}
+          >
+            <MenuItem eventKey="1">20x10</MenuItem>
+            <MenuItem eventKey="2">50x30</MenuItem>
+            <MenuItem eventKey="3">70x50</MenuItem>
+          </DropdownButton>
+        </ButtonToolbar>
+      </div>
+    )
+  }
+}
         class Main extends React.Component
         {
           constructor()
@@ -72,7 +112,7 @@ class Grid extends React.Component {
             gridCopy[row][col]=!gridCopy[row][col];
             this.setState({
               gridFull:gridCopy
-            })
+            });
           }
 
           seed=()=>{
@@ -90,14 +130,86 @@ class Grid extends React.Component {
               gridFull:gridCopy
             });
           }
+          playButton=()=>{
+            clearInterval(this.intervalId);
+            this.intervalId=setInterval(this.play,this.speed);
+          }
+          pauseButton=()=>{
+            clearInterval(this.intervalId);
+          }
+          slow=()=>{
+            this.speed=1000;
+            this.playButton();
+          }
+          fast=()=>{
+            this.speed=100;
+            this.playButton();
+          }
+          clear=()=>{
+            var grid=Array(this.rows).fill().map(()=>Array(this.cols).fill(false));
+            this.setState({
+              gridFull:grid,
+              generation:0
+            });
+          }
+          gridSize=(size)=>{
+            switch (size) {
+              case "1":
+              this.cols=20;
+              this.rows=10;
+                break;
+                case "2":
+                this.cols=50;
+                this.rows=30;
+                  break;
+                default:
+                this.cols=70;
+                this.rows=50;
+            }
+            this.clear();
+          }
+          play=()=>{
+            let g=this.state.gridFull;
+            let g2=arrayClone(this.state.gridFull);
+            for (let i = 0; i < this.rows; i++) {
+		            for (let j = 0; j < this.cols; j++) {
+		                let count = 0;
+		                  if (i > 0) if (g[i - 1][j]) count++;
+		                  if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		                  if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		                  if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		                  if (j > 0) if (g[i][j - 1]) count++;
+		                  if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		                  if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		                  if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+		                  if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		                  if (!g[i][j] && count === 3) g2[i][j] = true;
+		                                    }
+		                                    }
+                                        this.setState({
+                              		  gridFull: g2,
+                              		  generation: this.state.generation + 1
+                              		});
+          }
+
           componentDidMount(){
             this.seed();
+            this.playButton();
           }
           render()
           {
             return(
               <div>
                 <h1>The Game of Life</h1>
+                <Buttons
+                  playButton={this.playButton}
+                  pauseButton={this.pauseButton}
+                  slow={this.slow}
+                  fast={this.fast}
+                  clear={this.clear}
+                  seed={this.seed}
+                  gridSize={this.gridSize}
+                />
                 <Grid
                   gridFull={this.state.gridFull}
                   rows={this.rows}
